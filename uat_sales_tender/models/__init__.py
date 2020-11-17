211381df615c4d51f07903299bbf5080129c581b
ahmed <Oudounce.11@gmail.com>
2020-11-17 14:53:25 +0300

    mm

--- uat_sales_tender/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import sales_tender
-from . import tender_documents
-from . import product_tender_documents
-from . import supplier_tender
-from . import supplier_documents
